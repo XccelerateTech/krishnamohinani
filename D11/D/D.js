c4ede54bccb619530bf741e3df535d1c9b5cb512
Krishna Mohinani <krishnamohinani1994@gmail.com>
2018-10-11 15:10:51 +0800

    post lunch update

--- D11/D/D.js
+++ D11/D/D.js
@@ -6,14 +6,15 @@ $(function(){
         let long=$('input[name=lng]').val();
         let lat=$('input[name=lat]').val();
 
-        $.ajax(`https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&formatted=0`).done(function(data){
+        $.ajax(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}&formatted=0`).done(function(data){
 
         console.log(data);
         
         let sunrise = new Date(data.results.sunrise)
         let sunset = new Date(data.results.sunset)
 
-        $('#times').append(`<p>Sunrise is at: `)
+        $('#result').append(`<p>Sunrise is at: ${sunrise}</p>`);
+        $('#result').append(`<p>Sunset is at ${sunset}</p>`);
         })
     })
 })
